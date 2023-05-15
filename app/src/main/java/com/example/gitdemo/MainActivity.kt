71ba88ed3317ea32e678b170de5398bf4c7205a2
Tim-Desktop <timothy.fischer@skyfish.io>
2023-05-15 10:29:18 +0530

    demo initial commit

--- app/src/main/java/com/example/gitdemo/MainActivity.kt
+++ app/src/main/java/com/example/gitdemo/MainActivity.kt
@@ -7,5 +7,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        ///
+        val x = 9
+        val y = 10
+        val z = 11
     }
 }
